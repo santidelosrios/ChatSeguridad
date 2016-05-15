c2a3c4ea799996f0b33985fcaf3929185571837e
santidelosrios <sdelosrios95@gmail.com>
2016-05-14 22:12:11 -0500

    Diffie Hellman. By S

--- ChatSeguroCliente/src/modelo/Cliente.java
+++ ChatSeguroCliente/src/modelo/Cliente.java
@@ -6,11 +6,18 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
+import javax.crypto.KeyAgreement;
+import javax.crypto.SecretKey;
 import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.DHParameterSpec;
 
@@ -104,6 +111,7 @@ public class Cliente {
 		abrirSocket();
 		boolean coneccion = false;
 		escribirTCP("Hola");
+		doDiffieHellman();
 		String cdn = lecturaTCP();
 		if (cdn.equals("Ok")){
 			escribirTCP("iniciar");
@@ -118,23 +126,42 @@ public class Cliente {
 		}
 		return coneccion;
 	}
-	public void generateKeyPair() {
+	public void doDiffieHellman() {
 		String algorithm = "DH"; 
 		try {
 			//Se genera una signatura de 1024-bits para el algoritmo Diffie Hellman -- código adaptado de: https://examples.javacodegeeks.com/core-java/security/diffie-helman-key-pair-generation-and-parameters/
 			KeyPairGenerator keyG = KeyPairGenerator.getInstance(algorithm);
 			keyG.initialize(1024);
 			KeyPair pair = keyG.generateKeyPair();
-			//Se va a generar los parametros G,P y L -- código adaptado de: http://docstore.mik.ua/orelly/java-ent/security/ch13_07.htm
+			//Se va a generar los parametros G,P y L y se envian al servidor -- código adaptado de: http://docstore.mik.ua/orelly/java-ent/security/ch13_07.htm
 			//Class dh = Class.forName("javax.crypto.spec.DHParameterSpec");
 			DHParameterSpec dhspecs = ((DHPublicKey) pair.getPublic()).getParams();
 			BigInteger g = dhspecs.getG();
 			BigInteger p = dhspecs.getP();
 			int l = dhspecs.getL();
-			//Se genera la clave publica del cliente
+			String sg = g.toString();
+			String sp = p.toString();
+			String sl = String.valueOf(l);
+			String paramMessage = sp + ";" + sp + ";" + sl;
+			System.out.println("ParamMessageC:" + paramMessage);
+			escribirTCP(paramMessage);
+			//Se genera la clave publica del cliente y se envia al servidor
 			PublicKey pk = pair.getPublic(); 
 			byte[] clientKey = pk.getEncoded(); 
-			
+			escribirTCP(clientKey.toString());
+			System.out.println("Public KeyC: " + clientKey.toString());
+			//Se procede a hacer la negociacion de clave
+			KeyAgreement ka = KeyAgreement.getInstance(algorithm);
+			PrivateKey ppk = pair.getPrivate();
+			ka.init(ppk);
+			//Ahora se debe leer la clave publica del servidor para generar la llave secreta
+			String spk = lecturaTCP();
+			byte[] bspk = spk.getBytes();
+			KeyFactory kf = KeyFactory.getInstance(algorithm);
+			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(bspk);
+			PublicKey spublickey = kf.generatePublic(x509EncodedKeySpec);
+			ka.doPhase(spublickey, true);
+			SecretKey sk = ka.generateSecret("AES");
 			
 		} catch (NoSuchAlgorithmException nsa) {
 			// TODO Auto-generated catch block
@@ -144,6 +171,13 @@ public class Cliente {
 		//catch (ClassNotFoundException cne){
 		//	System.out.println("No existe la clase");
 		//}
+		catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public String getTextoEnviado() {
--- ChatSeguroServidor/src/modelo/HiloConector.java
+++ ChatSeguroServidor/src/modelo/HiloConector.java
@@ -3,11 +3,23 @@ package modelo;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
+import javax.crypto.KeyAgreement;
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.DHParameterSpec;
 
 import control.Ejecutable;
 import control.HiloEscucharChat;
@@ -39,6 +51,7 @@ public class HiloConector implements Runnable {
 				String recibirCliente = entrada.readUTF();
 				if(recibirCliente.equals("Hola")){
 					salida.writeUTF("Ok");
+					
 				}
 				recibirCliente= entrada.readUTF();
 				if(recibirCliente.equals("iniciar")){
@@ -64,7 +77,58 @@ public class HiloConector implements Runnable {
 		}
 	}
 	
-	public void generateKeyPair() {
+	public void doDiffieHellman(DataInputStream entrada, DataOutputStream salida) throws IOException {
+		//Se reciben los parametros G,P y L del cliente
+		String paramMessage = entrada.readUTF();
+		System.out.println("ParamMessageS:" + paramMessage);
+		String [] params = paramMessage.split(";");
+		String sg = params[0];
+		String sp = params[1];
+		String sl = params[2];
+		BigInteger g = new BigInteger(sg);
+		BigInteger p = new BigInteger(sp);
+		int l = new Integer(sl);
+		//Se recibe la clave publica del cliente
+		String publicKey = entrada.readUTF();
+		System.out.println("PublicKey del cliente: " + publicKey);
+		byte [] clientKey = publicKey.getBytes();
+		//Se procede a crear la llave publica y privada del servidor
+		String algorithm = "DH";
+		try {
+			KeyPairGenerator keyG = KeyPairGenerator.getInstance(algorithm);
+			DHParameterSpec dhSpec = new DHParameterSpec(p, g, l);
+			keyG.initialize(dhSpec);
+			KeyPair pair = keyG.generateKeyPair();
+			PublicKey pk = pair.getPublic(); 
+			byte [] bpk = pk.getEncoded();
+			String spk = bpk.toString();
+			//Se envia la llave publica al cliente
+			salida.writeUTF(spk);
+			//Se procede con la negociacion de clave y la generacion de la clave secreta
+			KeyAgreement ka = KeyAgreement.getInstance(algorithm);
+			PrivateKey ppk = pair.getPrivate();
+			ka.init(ppk);
+			KeyFactory kf = KeyFactory.getInstance(algorithm);
+			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(clientKey);
+			PublicKey cpk = kf.generatePublic(x509EncodedKeySpec);
+			ka.doPhase(cpk, true);
+			
+			SecretKey sk = ka.generateSecret("AES");
+			
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
